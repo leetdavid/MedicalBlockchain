9c6bb1372b932b453c291b734d409c02b9716b43
David Lee <leetdavid@outlook.com>
2018-03-09 07:16:12 +0800

    Deleting elements now work properly

--- routes/patientfunction.js
+++ routes/patientfunction.js
@@ -9,33 +9,33 @@ router.get('/', (req, res, next) => {
     'address': '0x0B45548a180a80B1d47A07b924491e0d3026E915',
     'status': 'REQUESTED'
   }, (err, requests) => {
+    console.log('dug out: ' + requests);
     res.render('patientfunction', { title: 'Patient Function' , requests: requests});
-  });
-});
-
-router.post('/delete', (req, res, next) => {
-  let dur = req.body.durp || req.query.durp;
-  console.log(dur);
-  Request.remove({
-    duration: dur
-  }, (err, result) => {
-    if(err) throw err;
-    else {
-      res.redirect('/patientfunction');
-    }
   });
 });
 
 router.post('/approve', (req, res, next) => {
   let dur = req.body.durp || req.query.durp;
+  console.log('APPROVE dur: ' + dur);
   Request.findOne({
     duration: dur
   }, (err, result) => {
-    if(err) throw err;
-    else {
-      result.status = 'APPROVED';
+    if(err) {
+      console.log(result);
       res.redirect('/patientfunction');
     }
+    result.status = 'APPROVED';
+    res.redirect('/patientfunction');
+  });
+});
+
+router.post('/delete', (req, res, next) => {
+  let dur = req.body.durp || req.query.durp;
+  console.log('DELETE dur: ' + dur);
+  Request.remove({
+    duration: dur
+  }, (err, result) => {
+    res.redirect('/patientfunction');
   });
 });
 
