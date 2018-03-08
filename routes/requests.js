748ff8f85f1d8475d239497c0c29cdddb0da5d83
David Lee <leetdavid@outlook.com>
2018-03-09 02:19:00 +0800

    Added Request History page

--- routes/requests.js
+++ routes/requests.js
@@ -6,13 +6,34 @@ let RequestHistory = require('../models/requesthistory');
 /* GET request page. */
 router.get('/', (req, res, next) => {
   RequestHistory.find({}, (err, requesthistory) => {
-    res.render('requests', { title: 'Express', requests: requesthistory});
+    res.render('requests', { title: 'Medichain - Request History', requests: requesthistory});
   });
 });
 
+router.get('/new', (req, res, next) => {
+  res.render('requests_add', {title: 'Medichain - Add Request'});
+});
+
 /* POST request page. */
-router.post('/', (req, res, next) => {
-  
+router.post('/new', (req, res, next) => {
+  let newEntry = {
+    "sourceName": req.body.entry.sourceName,
+    "sourceURL": null,
+    "status": req.body.entry.status,
+    "requestdate": req.body.entry.requestdate,
+    "transactiondate": req.body.entry.transactiondate,
+    "city": req.body.entry.city,
+    "country": req.body.entry.country
+  };
+
+  RequestHistory.create(newEntry, (err, doc) => {
+    if(err) throw err;
+    console.log('new entry inserted:'+newEntry);
+    res.redirect('/requests');
+    // RequestHistory.find({}, (err, history) => {
+      
+    // });
+  })
 });
 
 module.exports = router;
